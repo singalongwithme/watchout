f01aad83143977f1393c3626c20b36b332afb50c
singalongwithme <csinghavong@gmail.com>
2014-08-02 11:13:56 -0700

    made enemy dots

--- watchout.js
+++ watchout.js
@@ -1,2 +1,11 @@
-// start slingin' some d3 here.
+// make enemies
+var enemyArray = [];
+for( var i = 0; i < 30; i++ ){
+  var rand = Math.random() * 720;
+  enemyArray[i] = rand;
+}
 
+d3.select('.enemyGroup').selectAll('.enemies').data([1]).enter()
+.append('circle').classed('enemies', true).attr('cx', function(data){
+return data;}).attr('cy', function(data){return Math.random() * 400})
+.attr('r', 6).attr('fill', 'teal');
